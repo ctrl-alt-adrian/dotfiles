64e69b5b37d8b4ebe19a1e164fd0c858da0ce78f
Adrian Thomas <adrianthomas@Adrians-MacBook-Air.local>
2016-03-08 15:26:28 -0600

    Added keys

--- .keybindings.el
+++ .keybindings.el
@@ -1,11 +1,14 @@
 ;; KEYBINDINGS!!!!
 
-;; Neotree toggle
+;;======== Neotree ========;;
 (global-set-key [f8] 'neotree-toggle)
-
-(require 'multiple-cursors)
-;; multiple-cursors keybindings
+;;======== multiple-cursors ========;;
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+;; ========== Org Mode ===========;;
+;; key binding to bring up agenda list
+(define-key global-map (kbd "C-c t a") 'air-pop-to-org-agenda)
+;; Close capture key binding
+(define-key global-map (kbd "C-c c") 'air-org-task-capture)
