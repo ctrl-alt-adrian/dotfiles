fdf88adfacc333e9ec7a24272490dbaaab2c970c
Adrian <adrian.l.thomas@outlook.com>
2023-04-18 09:02:44 -0500

    removed unused test runners

--- NvChad/custom/configs/neotest.lua
+++ NvChad/custom/configs/neotest.lua
@@ -1,8 +1,15 @@
 local opts = {
+
   adapters = {
-    require("neotest-python"),
     require("neotest-go"),
-    require("neotest-jest"),
+    require("neotest-jest").setup({
+      jestCommand = "npm test --",
+      jestConfigFile = "custom.jest.config.ts",
+      env = { CI = true},
+      cwd = function()
+        return vim.fn.getcwd()
+      end,
+    }),
     require("neotest-rust"),
   }
 }
