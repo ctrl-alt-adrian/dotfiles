d4c3d4fa110428999541c91af09c825796223588
Adrian <adrian.l.thomas@outlook.com>
2023-05-09 12:56:17 -0500

    fix null-ls formatting on save

--- NvChad/custom/configs/lspconfig.lua
+++ NvChad/custom/configs/lspconfig.lua
@@ -1,42 +1,56 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
-local util = require "lspconfig/util"
+local util = require("lspconfig/util")
 
-local servers = {"html", "cssls", "clangd", "volar", "gopls", "svelte", "tsserver", "eslint", "emmet_ls", "pyright", "unocss", "jsonls" }
+local servers = {
+  "html",
+  "cssls",
+  "clangd",
+  "volar",
+  "gopls",
+  "svelte",
+  "tsserver",
+  "eslint",
+  "emmet_ls",
+  "pyright",
+  "unocss",
+  "jsonls",
+  "lua_language_server",
+}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp].setup({
     on_attach = on_attach,
     capabilities = capabilities,
-  }
+  })
 end
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function (ev)
-  -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-  -- Buffer Local mappings
-    local opts = { buffer = ev.buf}
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    -- Buffer Local mappings
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format({ async = true })
     end, opts)
-  end
+  end,
 })
 
 -- golang config
-lspconfig.gopls.setup{
+lspconfig.gopls.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = { "gopls"},
-  filetypes = { "go", "gomod", "gowork", "gotmpl"},
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
     gopls = {
@@ -44,9 +58,9 @@ lspconfig.gopls.setup{
       usePlaceholders = true,
       analyses = {
         unusedparams = true,
-      }
-    }
-  }
-}
+      },
+    },
+  },
+})
 
 -- typescript cofig
--- NvChad/custom/plugins.lua
+++ NvChad/custom/plugins.lua
@@ -66,7 +66,7 @@ local plugins = {
     ft = "go",
     dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
-      require("dap-go").seutp(opts)
+      require("dap-go").setup(opts)
       require("core.utils").load_mappings("dap_go")
     end
   },
