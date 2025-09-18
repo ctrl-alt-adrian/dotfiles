22c96cf703989b46fbed51cc974a352be62c5a5a
Adrian Thomas <adrian.l.thomas@outlook.com>
2025-09-18 08:45:00 -0500

    updates configs

--- nvim/.config/nvim/lua/plugins/mcp-hub.lua
+++ nvim/.config/nvim/lua/plugins/mcp-hub.lua
@@ -7,4 +7,20 @@ return {
   config = function()
     require("mcphub").setup()
   end,
+  keys = {
+    { "<leader>mt", ":MCPHub<CR>", desc = "Open MCP Hub" }, -- official UI command
+    { "<leader>mh", ":checkhealth mcphub<CR>", desc = "Check health" }, -- health checker
+    {
+      "<leader>mr",
+      function()
+        local hub = require("mcphub").get_hub_instance()
+        if hub then
+          hub:restart()
+        else
+          vim.notify("MCPHub not ready", vim.log.levels.WARN)
+        end
+      end,
+      desc = "Restart Hub",
+    },
+  },
 }
