3b5f93d1c27f80eac2653c1e34a0962343a298ee
Adrian Thomas <adrian.l.thomas@outlook.com>
2025-11-06 09:43:29 -0600

    update dotfiles

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -1,67 +1,76 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      gopls = {
-        settings = {
-          gopls = {
-            gofumpt = true,
-            codelenses = {
-              gc_details = false,
-              generate = true,
-              regenerate_cgo = true,
-              run_govulncheck = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
             },
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
-            analyses = {
-              nilness = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
-            },
-            usePlaceholders = true,
-            completeUnimported = true,
-            staticcheck = true,
-            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-            semanticTokens = true,
           },
         },
       },
     },
-    setup = {
-      gopls = function(_, opts)
-        -- Override on_attach to compose with LazyVim's default and add the semantic tokens workaround
-        opts.on_attach = function(client, bufnr)
-          -- Call LazyVim's default on_attach first (using updated Snacks API)
-          require("snacks.util.lsp").on_attach(client, bufnr)
-
-          -- Workaround for gopls not supporting semanticTokensProvider
-          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-          if not client.server_capabilities.semanticTokensProvider then
-            local semantic = client.config.capabilities.textDocument.semanticTokens
-            client.server_capabilities.semanticTokensProvider = {
-              full = true,
-              legend = {
-                tokenTypes = semantic.tokenTypes,
-                tokenModifiers = semantic.tokenModifiers,
-              },
-              range = true,
-            }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      -- Add LspAttach autocmd for gopls-specific customization
+      -- This composes with LazyVim's default on_attach instead of replacing it
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("GoplsSemanticTokens", { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client.name == "gopls" then
+            -- Workaround for gopls not supporting semanticTokensProvider
+            -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+            if not client.server_capabilities.semanticTokensProvider then
+              local semantic = client.config.capabilities.textDocument.semanticTokens
+              if semantic then
+                client.server_capabilities.semanticTokensProvider = {
+                  full = true,
+                  legend = {
+                    tokenTypes = semantic.tokenTypes,
+                    tokenModifiers = semantic.tokenModifiers,
+                  },
+                  range = true,
+                }
+              end
+            end
           end
-        end
-      end,
-    },
+        end,
+      })
+    end,
   },
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,34 +1,47 @@
 return {
-  vim.lsp.enable({
-    "gopls",
-    "lua_ls",
-  }),
-  vim.diagnostic.config({
-    virtual_lines = true,
-    -- virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-      border = "rounded",
-      source = true,
-    },
-    signs = {
-      text = {
-        [vim.diagnostic.severity.ERROR] = "󰅚 ",
-        [vim.diagnostic.severity.WARN] = "󰀪 ",
-        [vim.diagnostic.severity.INFO] = "󰋽 ",
-        [vim.diagnostic.severity.HINT] = "󰌶 ",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- Disable inlay hints globally
+      inlay_hints = {
+        enabled = false,
       },
-      numhl = {
-        [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-        [vim.diagnostic.severity.WARN] = "WarningMsg",
+
+      -- Diagnostic configuration
+      diagnostics = {
+        virtual_lines = true,
+        -- virtual_text = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+          source = true,
+        },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+          },
+          numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+          },
+        },
       },
     },
-  }),
-  vim.lsp.inlay_hint.enable(false),
-  vim.keymap.set("n", "<leader>ih", function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
-  end, { desc = "Toggle Inlay Hints" }),
+    keys = {
+      -- Toggle inlay hints for current buffer
+      {
+        "<leader>ih",
+        function()
+          local bufnr = vim.api.nvim_get_current_buf()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+        end,
+        desc = "Toggle Inlay Hints",
+      },
+    },
+  },
 }
